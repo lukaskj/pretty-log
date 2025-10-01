a52aa4012100ffce00a595204aac4e7f247239f4
Lukas K Jacomin <lukasjacomin@gmail.com>
2025-10-01 19:05:49 -0300

    chore: help message

--- src/getOpt.ts
+++ src/getOpt.ts
@@ -5,29 +5,29 @@ const defaultOptions: TOptions = {
   maxLines: 6,
 };
 
-export function getOptions(): TOptions {
-  const argsConfig = {
-    options: {
-      maxLines: {
-        type: "string",
-        short: "m",
-        description: `Maximum number of lines to display for objects (default: ${defaultOptions.maxLines})`,
-      },
-      trim: {
-        type: "string",
-        short: "t",
-        description:
-          'Trim string to strip from the start of each line (default: none, e.g. use ">  " for logs prefixed with "> ")',
-        multiple: true,
-      },
-      help: {
-        type: "boolean",
-        short: "h",
-        description: "Show this help message",
-      },
+const argsConfig = {
+  options: {
+    maxLines: {
+      type: "string",
+      short: "m",
+      description: `Maximum number of lines to display for objects (default: ${defaultOptions.maxLines})`,
     },
-    allowPositionals: false,
-  };
+    trim: {
+      type: "string",
+      short: "t",
+      description:
+        'Trim string to strip from the start of each line (default: none, e.g. use ">  " for logs prefixed with "> ")',
+      multiple: true,
+    },
+    help: {
+      type: "boolean",
+      short: "h",
+      description: "Show this help message",
+    },
+  },
+  allowPositionals: false,
+};
+export function getOptions(): TOptions {
   // biome-ignore lint/suspicious/noExplicitAny: whatever
   const args = parseArgs(argsConfig as any).values;
 
@@ -37,22 +37,44 @@ export function getOptions(): TOptions {
     ...args,
   };
 
-  const helpLines: string[] = [];
-
   if (args.help) {
-    for (const _key in argsConfig.options) {
-      const key = _key as keyof typeof argsConfig.options;
-      const opt = argsConfig.options[key];
-      helpLines.push(
-        `  ${"short" in opt ? `-${opt.short}, ` : ""}--${key} ${opt.type === "boolean" ? "" : `<${opt.type}>`}  ${opt.description}`,
-      );
-    }
-    console.log(`Usage: pretty-log [options]
-
-Options:\n`);
-    console.log(helpLines.join("\n"));
+    console.log("Usage: pretty-log [options]\n\nOptions:");
+    console.log(getHelpMessage());
     process.exit(0);
   }
 
   return options;
+}
+
+function getHelpMessage(): string {
+  return Object.entries(argsConfig.options)
+    .map(([key, opt]) => {
+      const shortFlag = "short" in opt ? ` -${opt.short}, ` : "     ";
+      const fullFlag = `--${key}`;
+      const value = opt.type === "boolean" ? "" : "<value>";
+      const command = `${shortFlag}${fullFlag}`;
+
+      return {
+        command,
+        value,
+        description: opt.description,
+        fullLength: command.length + value.length,
+      };
+    })
+    .reduce(
+      (acc, { command, value, description, fullLength }) => {
+        const maxLength = Math.max(...acc.map((item) => item.fullLength));
+        const paddedCommand = `${command.padEnd(maxLength - value.length, " ")} ${value}`;
+
+        acc.push({
+          fullLength,
+          line: `${paddedCommand.padEnd(maxLength, " ")} ${description}`,
+        });
+
+        return acc;
+      },
+      [] as Array<{ fullLength: number; line: string }>,
+    )
+    .map(({ line }) => line)
+    .join("\n");
 }
